2d4deed642768e7cf913b3823407c4a94a2c0bb8
iamghost <gkmangezi09@email.com>
2026-07-10 20:47:58 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.log("Service Worker failed:", err));
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
